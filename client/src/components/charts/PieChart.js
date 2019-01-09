76d3a4564ff9688c5ea9939547047aac43cdfcc4
Mohamed Bouchi <mohamedbouchi@Mohameds-iMac.local>
2019-01-09 21:01:30 +0100

    charts colors

--- client/src/components/charts/PieChart.js
+++ client/src/components/charts/PieChart.js
@@ -1,21 +1,28 @@
-import { PieChart, Pie, Cell, Tooltip} from 'recharts';
+import { PieChart, Pie, Cell, Tooltip, Legend} from 'recharts';
 import React, { Component } from 'react';
 
 const data = [{name: 'Timo', value: 20}, {name: 'Arndt', value: 20},
                   {name: 'Ralf', value: 30}, {name: 'Mohamed', value: 10}];
 const data01 = [{name: 'Inprocess', value: 1},{name: 'Open', value: 5},{name: 'waiting', value: 7},{name: 'finished', value: 10}]
 const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+const COLORS01 = ['#8dd1e1', '#83a6ed', '#82ca9d', '#d0ed57'];
 
 
 export default class PieCharts extends Component {
 render () {
     return (
       <PieChart width={500} height={210} >
-    <Pie dataKey="value" data={data01} cx={120} cy={90} outerRadius={60} fill="#8884d8"/>
+    <Pie dataKey="value" data={data01} cx={110} cy={110} outerRadius={60} fill="#8884d8">
+    {
+      data01.map((entry, index) => (
+        <Cell key={`cell-${index}`} fill={COLORS01[index]}/>
+      ))
+    }
+  </Pie>
       <Pie dataKey="value"
         data={data} 
-        cx={120} 
-        cy={90} 
+        cx={110} 
+        cy={110} 
         innerRadius={60}
         outerRadius={80} 
         fill="#8884d8"
@@ -28,6 +35,7 @@ render () {
         
       </Pie>
      <Tooltip />
+     <Legend align='left' layout='vertical'/>
     </PieChart>
   );
 }
