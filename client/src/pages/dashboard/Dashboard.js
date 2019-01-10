de06e885588eedf8f4a2167a404fd4d1dcd68b46
Mohamed Bouchi <mohamedbouchi@Mohameds-iMac.local>
2019-01-10 20:47:38 +0100

    test request in dashboard

--- client/src/pages/dashboard/Dashboard.js
+++ client/src/pages/dashboard/Dashboard.js
@@ -2,17 +2,30 @@ import React, {Component} from 'react';
 import Content from '../../components/content/Content';
 import Grid from '@material-ui/core/Grid';
 import './dashboard.css';
-import SideDrawer from '../../components/SideDrawer';
 export default class Dashboard extends Component {
-  constructor(props){
-    super(props);
-  }
 
   render() {
+    const request = {
+      query: `
+        query{
+          login(email:"user4@test.com",password:"123") {token}
+          }
+      `
+    };
+    fetch('http://localhost:3000/graphql', {
+      method: 'POST',
+      body: JSON.stringify(request),
+      headers:{
+        'Content-Type':'application/json'
+      }
+    }).then(data => {
+      return data.json();
+    }).then(res => {
+      console.log(res);
+    });
     return (
       <main id='main'>
         <Grid container spacing={0}>
-          {/* <SideDrawer></SideDrawer> */}
           <Grid item lg={12} sm={12} xs={12} className='container'>
               <Content/>
           </Grid>
